0ecdc88a91d7464822303297fa3d29ce2b52d068
izzalDev <rizal.fadlullah@gmail.com>
2024-09-12 23:19:15 +0700

    use flutte template

--- flutter-android/idx-template.nix
+++ flutter-android/idx-template.nix
@@ -1,17 +1,61 @@
-# No user-configurable parameters
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Shell script that produces the final environment
-  bootstrap = ''
-    # Copy the folder containing the `idx-template` files to the final
-    # project folder for the new workspace. ${./.} inserts the directory
-    # of the checked-out Git folder containing this template.
-    cp -rf ${./.} "$out"
+  # Which nixpkgs channel to use.
+  channel = "stable-23.11"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.nodePackages.firebase-tools
+    pkgs.jdk17
+    pkgs.unzip
+  ];
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      "Dart-Code.flutter"
+      "Dart-Code.dart-code"
+    ];
+    workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
+      onCreate = {
+        build-flutter = ''
+          cd /home/user/myapp/android
 
-    # Set some permissions
-    chmod -R +w "$out"
+          ./gradlew \
+            --parallel \
+            -Pverbose=true \
+            -Ptarget-platform=android-x86 \
+            -Ptarget=/home/user/myapp/lib/main.dart \
+            -Pbase-application-name=android.app.Application \
+            -Pdart-defines=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85NzU1MDkwN2I3MGY0ZjNiMzI4YjZjMTYwMGRmMjFmYWMxYTE4ODlhLw== \
+            -Pdart-obfuscation=false \
+            -Ptrack-widget-creation=true \
+            -Ptree-shake-icons=false \
+            -Pfilesystem-scheme=org-dartlang-root \
+            assembleDebug
 
-    # Remove the template files themselves and any connection to the template's
-    # Git repository
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
-  '';
+          # TODO: Execute web build in debug mode.
+          # flutter run does this transparently either way
+          # https://github.com/flutter/flutter/issues/96283#issuecomment-1144750411
+          # flutter build web --profile --dart-define=Dart2jsOptimization=O0 
+
+          adb -s localhost:5555 wait-for-device
+        '';
+      };
+      
+      # To run something each time the workspace is (re)started, use the `onStart` hook
+    };
+    # Enable previews and customize configuration
+    previews = {
+      enable = true;
+      previews = {
+        android = {
+          command = ["flutter" "run" "--machine" "-d" "android" "-d" "localhost:5555"];
+          manager = "flutter";
+        };
+      };
+    };
+  };
 }
