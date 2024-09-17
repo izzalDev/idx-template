d42d82a864a5bc15035eb58f2bfff0b566d7b754
izzalDev <rizal.fadlullah@gmail.com>
2024-09-17 10:44:59 +0700

    update environment

--- flutter-android/idx-template.nix
+++ flutter-android/idx-template.nix
@@ -1,18 +1,28 @@
-{pkgs,...}: {
-    packages = [
-        pkgs.git
-        pkgs.zulu17
-        pkgs.gradle
-        pkgs.starship
-        pkgs.flutter
-        pkgs.gh
-        pkgs.pre-commit
-    ];
-    bootstrap = ''
-        echo 'eval "$(starship init bash)"' >> ~/.bashrc
-        flutter create "$out" --platforms="android" --project-name="${projectName}"
-        mkdir "$out"/.idx
-        cp ${./dev.nix} "$out"/.idx/dev.nix
-        install --mode u+rw ${./dev.nix} "$out"/.idx/dev.nix
-    '';
+{ pkgs, projectName, ... }: {
+  packages = [
+    pkgs.git
+    pkgs.zulu17
+    pkgs.gradle
+    pkgs.starship
+    pkgs.flutter
+    pkgs.gh
+    pkgs.pre-commit
+  ];
+
+  bootstrap = ''
+    # Add starship prompt initialization to bashrc
+    echo 'eval "$(starship init bash)"' >> ~/.bashrc
+
+    # Create a new Flutter project
+    flutter create "$out" --platforms=android --project-name="${projectName}"
+
+    # Create a hidden index directory inside the output
+    mkdir -p "$out/.idx"
+
+    # Copy dev.nix to the index directory
+    cp ${./dev.nix} "$out/.idx/dev.nix"
+
+    # Set appropriate permissions for dev.nix
+    install -m u+rw ${./dev.nix} "$out/.idx/dev.nix"
+  '';
 }
